cd2e6f5d526b5fb3ff80a8c8bbd9fde6cbf55999
DESKTOP-MQS8GBN\hr7te <hriteshbebop@gmail.com>
2021-03-10 15:16:59 +0530

    subscription added

--- redux-basics.js
+++ redux-basics.js
@@ -5,6 +5,7 @@ const initialState = {
   counter: 0
 }
 
+
 // Reducer
 const rootReducer = (state = initialState, action) => {
   if (action.type === 'INC_COUNTER') {
@@ -22,15 +23,19 @@ const rootReducer = (state = initialState, action) => {
   return state;
 };
 
+
 // Store
 const store = createStore(rootReducer);
 console.log(store.getState());
+
+
+// Subscription
+store.subscribe(() => {
+  console.log('[Subscription]', store.getState());
+});
 
 
 // Dispatching action
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
-
-
-// Subscription
